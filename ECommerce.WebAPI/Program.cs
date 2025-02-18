a4c673225c83838118266a68ac95f4cbcb3e8a92
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2025-02-18 22:38:10 +0300

    Frontend...

---
+++ ECommerce.WebAPI/Program.cs
@@ -0,0 +1,39 @@
+using ECommerce.DataAccessLayer.Abstract;
+using ECommerce.DataAccessLayer.Context;
+using ECommerce.DataAccessLayer.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//builder.Services.AddScoped<IGenericDal, GenericRepository>();
+builder.Services.AddScoped<IProductDal, ProductRepository>();
+builder.Services.AddScoped<IOrderDal, OrderRepository>();
+builder.Services.AddScoped<IOrderDetailDal, OrderDetailRepository>();
+builder.Services.AddScoped<IUserDal, UserRepository>();
+
+builder.Services.AddDbContext<ECommerceContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
